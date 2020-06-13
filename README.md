4e69c3a9809ffef0ed113c573ec00b6fae1eefb9
Ananya Vaka <Ananya.Vaka@momentumcc.com>
2020-06-13 15:56:57 -0400

    Updates

--- README.md
+++
@@ -1,6 +0,0 @@
-# Rewards
-
-To install node modules, run "npm install"
-To start application, run "npm start"
-
-App URL: 
--- src/_reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import { rewards, categories } from './rewards.reducer';
-
-const rootReducer = combineReducers({
-  rewards, categories
-});
-
-export default rootReducer;
